9e38a9932008e31bbddf03a3deceaeadd14657e7
DVs07 <aldopablo7@hotmail.com>
2024-11-04 19:20:50 -0300

    Se agregaron las funcionalidades a los botones de editar y elimar en la seccion de clientes, se agruparon funciones en un solo archivo para no repetir codigo.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 (function() {
     let DB;
+
+    const listadoClientes = document.querySelector('#listado-clientes');
     
     document.addEventListener('DOMContentLoaded', () =>{
         crearDB();
@@ -7,6 +9,8 @@
         if(window.indexedDB.open('crm', 1)){
             obtenerClientes();
         }
+
+        listadoClientes.addEventListener('click', eliminarRegistro);
     });
 
     // Crear la base de datos en la version 1.0
@@ -46,6 +50,36 @@
 
     }
 
+    function eliminarRegistro(e){
+        // console.log(e.target.classList);
+        if(e.target.classList.contains('eliminar')){
+            // console.log('Hiciste clic en Eliminar');
+            const idEliminar = Number(e.target.dataset.cliente);
+
+            // console.log(idEliminar);
+
+            const confirmar = confirm('Desea eliminar este Cliente?')
+            
+            // console.log(confirmar);
+            if(confirmar){
+                const transaction = DB.transaction(['crm'], 'readwrite');
+                const objectStore = transaction.objectStore('crm');
+
+                objectStore.delete(idEliminar);
+
+                transaction.oncomplete = function (){
+                    console.log('ELiminado correctamente');
+                    e.target.parentElement.parentElement.remove();
+                }
+
+                transaction.onerror = function(){
+                    console.log('Hubo un error');
+                    
+                }
+            }
+        }
+        
+    }
     function obtenerClientes(){
         const abrirConexion = window.indexedDB.open('crm',1);
 
@@ -70,24 +104,23 @@
                     
                     const {nombre, email, telefono, empresa, id} = cursor.value; // Destructuring. Asignar los valores de la base de datos a las variables.
 
-                    const listado = document.querySelector('#listado-clientes');
-
-                    listado.innerHTML += ` <tr>
-                                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                                    <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
-                                                    <p class="text-sm leading-10 text-gray-700"> ${email} </p>
-                                                </td>
-                                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
-                                                    <p class="text-gray-700">${telefono}</p>
-                                                </td>
-                                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
-                                                    <p class="text-gray-600">${empresa}</p>
-                                                </td>
-                                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
-                                                    <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                                                    <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
-                                                </td>
-                                            </tr>`;
+                    listadoClientes.innerHTML += 
+                        ` <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
+                                <p class="text-sm leading-10 text-gray-700"> ${email} </p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
+                                <p class="text-gray-700">${telefono}</p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
+                                <p class="text-gray-600">${empresa}</p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
+                                <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
+                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
+                            </td>
+                        </tr>`;
 
                     cursor.continue();
                 }else{
---
+++ js/funciones.js
@@ -0,0 +1,57 @@
+function conectarDB(){
+    const abrirConexion = window.indexedDB.open('crm', 1);
+
+    // Si hay un error
+    abrirConexion.onerror = function(){
+        console.log('Hubo un error');
+        
+    };
+
+    // Si todo sale bien
+    abrirConexion.onsuccess = function(){
+        DB = abrirConexion.result;
+
+        console.log('Base de datos abierta');
+    };
+}
+
+function imprimirAlerta(mensaje, tipo){
+
+    const alerta = document.querySelector('.alerta');
+    if(!alerta){
+        // Crear el div
+        const divAlerta = document.createElement('DIV');
+        divAlerta.classList.add('px-4', 'py-3', 'rounded', 'mx-auto', 'mt-6', 'text-center','border', 'alerta');
+
+
+        const icono = document.createElement('i');
+
+        if(tipo === 'error'){
+            divAlerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700' );
+            // icono.classList.add('fa', 'fa-exclamation-circle', 'me-2');
+            // icono.classList.add('custom-icon'); // Icono de error
+            // icono.classList.add('me-2', 'text-center');
+            // icono.innerHTML = `<svg class="w-6 h-6 text-red-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+            // <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v5a1 1 0 1 0 2 0V8Zm-1 7a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H12Z" clip-rule="evenodd" class="align-middle"/></svg>`;
+
+            
+        } else{
+            divAlerta.classList.add('bg-green-100', 'border-green-400', 'text-green-700');
+            // icono.classList.add('fa', 'fa-check-circle', 'me-2'); // Icono de éxito
+        }
+
+        const textoAlerta = document.createElement('span');
+        textoAlerta.textContent = mensaje;
+
+        
+        divAlerta.appendChild(textoAlerta);
+        divAlerta.appendChild(icono);
+        
+        formulario.appendChild(divAlerta);
+
+        setTimeout(() => {
+            divAlerta.remove();
+        }, 3000);
+        
+    }
+}
